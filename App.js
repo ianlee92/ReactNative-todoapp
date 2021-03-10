846a03160b669d1cb964f267750e722233f9d4cc
ianlee92 <ethian9227@gmail.com>
2021-03-10 11:12:17 +0900

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  const [OutputText, setOutputText] = useState("Hi there! 👀")
+  function buttonTapped(){
+    setOutputText("🎉 Welcome to React Native World!")
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{OutputText}</Text>
+      <Button title="Click" onPress={buttonTapped} />
       <StatusBar style="auto" />
     </View>
   );
